54a84f0c6009a500cc1a248ec3b09f5b82141852
Benjamin Rianda <reptarforlife@gmail.com>
2015-03-24 07:42:47 -0700

    done

---
+++ ui/mods/GrandPrixMapPack/maplist.js
@@ -0,0 +1,6 @@
+cShareSystems.load_pas("RR Grand Prix", [
+	"coui://ui/mods/GrandPrixMapPack/systems/[gp]metal_mania_circuit.pas",
+	"coui://ui/mods/GrandPrixMapPack/systems/[gp]nurburgring_raceway.pas",
+	"coui://ui/mods/GrandPrixMapPack/systems/[GP]Rainbow_Road.pas",
+	"coui://ui/mods/GrandPrixMapPack/systems/[gp]short_track.pas"
+]);
